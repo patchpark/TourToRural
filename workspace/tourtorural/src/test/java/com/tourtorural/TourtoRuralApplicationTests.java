4b26e894935554eebb6c12a492e794e00f53d6de
patchpark <kevinminsang99@gmail.com>
2023-08-10 16:26:04 +0900

    기본설정

---
+++ workspace/tourtorural/src/test/java/com/tourtorural/TourtoRuralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourtorural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourtoRuralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
